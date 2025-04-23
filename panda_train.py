36a33da4d6e721ded6763b9d5064d3b73d7be836
psan3333 <psan3333@mail.ru>
2025-04-23 17:50:06 +0700

    Launched agent for training

--- panda_train.py
+++ panda_train.py
@@ -38,7 +38,7 @@ def get_train_cfg(exp_name):
             "experiment_name": exp_name,
             "load_run": -1,
             "log_interval": 1,
-            "max_iterations": 1000,
+            "max_iterations": 100000,
             "num_steps_per_env": 24,
             "policy_class_name": "ActorCritic",
             "record_interval": -1,
